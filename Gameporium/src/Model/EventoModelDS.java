340e292409f21eb91472add54310c63f10b20c7e
NANISOREH <>
2019-05-21 01:41:20 +0200

    pulizia warnings

--- Gameporium/src/Model/EventoModelDS.java
+++ Gameporium/src/Model/EventoModelDS.java
@@ -5,7 +5,7 @@ import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Time;
+//import java.sql.Time;
 import java.util.Collection;
 import java.util.LinkedList;
 
