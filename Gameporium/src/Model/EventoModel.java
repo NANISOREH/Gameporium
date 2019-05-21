9c0148d4f7e2b36a564074e44a56f144d2d1fa1d
NANISOREH <>
2019-05-21 19:53:21 +0200

    creato branch di testing

--- Gameporium/src/Model/EventoModel.java
+++ Gameporium/src/Model/EventoModel.java
@@ -6,7 +6,7 @@ import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Time;
+//import java.sql.Time;
 import java.util.Collection;
 import java.util.LinkedList;
 
