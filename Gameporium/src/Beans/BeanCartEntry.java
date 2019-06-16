c079b468c05a9ccca4ef99ddf6b7f085ae8b7855
nico <nikbollettieri@gmail.com>
2019-06-16 11:36:10 +0200

    aggiunto bean per cartentry

---
+++ Gameporium/src/Beans/BeanCartEntry.java
@@ -0,0 +1,32 @@
+package Beans;
+
+import java.io.Serializable;
+
+public class BeanCartEntry  extends Bean implements Serializable
+{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	BeanProduct product;
+	int quantita;
+	
+	public BeanProduct getProduct() {
+		return product;
+	}
+	
+	@Override
+	public String toString() {
+		return "BeanCartEntry [product=" + product + ", quantita=" + quantita + "]";
+	}
+	public void setProduct(BeanProduct product) {
+		this.product = product;
+	}
+	public int getQuantita() {
+		return quantita;
+	}
+	public void setQuantita(int quantita) {
+		this.quantita = quantita;
+	}
+
+}
