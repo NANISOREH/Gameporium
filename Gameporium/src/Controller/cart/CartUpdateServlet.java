1264b81ef8dd16055939735a54f273d3b4916124
NANISOREH <>
2019-06-24 18:50:09 +0200

    nient

--- Gameporium/src/Controller/cart/CartUpdateServlet.java
+++ Gameporium/src/Controller/cart/CartUpdateServlet.java
@@ -6,7 +6,6 @@ import java.util.Collection;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -16,25 +15,14 @@ import Beans.BeanCartEntry;
 import Beans.BeanProduct;
 import Model.ProductModel;
 
-/**
- * Servlet implementation class CartRemove
- */
 public class CartUpdateServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	static ProductModel model = new ProductModel();
 
-	/**
-	 * @see HttpServlet#HttpServlet()
-	 */
 	public CartUpdateServlet() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
